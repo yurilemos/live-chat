c7cd30bf75437dfdec9235d84253b1c427efbf86
yurilemos <56370419+yurilemos@users.noreply.github.com>
2021-02-08 20:27:42 -0300

    commit on 'master'

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -3,7 +3,7 @@
         <div v-if="error">{{ error }}</div>
         <div v-if="documents" class="messages">
             <div v-for="doc in documents" :key="doc.id" class="single">
-                <span class="created-at">{{ doc.createdAt }}</span>
+                <span class="created-at">{{ doc.createdAt.toDate() }}</span>
                 <span class="name">{{ doc.name }}</span>
                 <span class="message">{{ doc.message }}</span>
             </div>
@@ -26,5 +26,25 @@ export default {
 </script>
 
 <style>
-
+    .chat-window {
+        background: #fafafa;
+        padding: 30px 20px;
+    }
+    .single {
+        margin: 18px 0;
+    }
+    .created-at {
+        display: block;
+        color: #999;
+        font-size: 12px;
+        margin-bottom: 4px;
+    }
+    .name {
+        font-weight: bold;
+        margin-right: 6px;
+    }
+    .messages {
+        max-height: 400px;
+        overflow: auto;
+    }
 </style>
