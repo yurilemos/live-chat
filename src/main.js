4d88b0d40ab178293e1b1fb636a8ab834c3845d0
yurilemos <56370419+yurilemos@users.noreply.github.com>
2021-02-08 16:04:20 -0300

    commit on 'master'

--- src/main.js
+++ src/main.js
@@ -4,4 +4,17 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+// import firebase autg service
+import { projectAuth } from './firebase/config'
+
+let app
+
+projectAuth.onAuthStateChanged(() => {
+    if (!app) {
+        app = createApp(App)
+            .use(router)
+            .mount('#app')
+    }
+})
+
+
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import NewChatForm from '../components/NewChatForm.vue'
+import getUser from '../composables/getUser'
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
-  components: { Navbar }
+  components: { Navbar, NewChatForm },
+  setup() {
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 
